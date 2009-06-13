e2d409c6722c362e899fafa9483a2247739e26f2
hirafoo <hirafoo@cpan.org>
2009-06-14 02:18:21 +0900

    use :after in migration

--- db/migration_helper.rb
+++ db/migration_helper.rb
@@ -4,4 +4,24 @@ module MigrationHelper
 
     execute "alter table #{from_table} add constraint #{constraint_name} foreign key (#{from_column}) references #{to_table}(id)"
   end
+
+
+
+module ActiveRecord
+  module ConnectionAdapters # :nodoc:
+    module SchemaStatements
+
+      def add_column_options!(sql, options) #:nodoc:
+        sql << " DEFAULT #{quote(options[:default], options[:column])}" if options_include_default?(options)
+        # must explicitly check for :null to allow change_column to work on migrations
+        if options[:null] == false
+          sql << " NOT NULL"
+        end
+        if options[:after]
+          sql << " AFTER #{options[:after]}"
+        end
+      end
+
+    end
+  end
 end
